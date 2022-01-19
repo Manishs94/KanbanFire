3bd65ca3d53a91e93e09ab7598f7a4609a081224
Manish Sondhi <manishsondhi01@gmail.com>
2022-01-19 18:29:18 -0500

    First Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJoA5vWKgYUI_9LJjgNCA3HgRkyAEkWGI",
+    authDomain: "kanban-fire-be71d.firebaseapp.com",
+    databaseURL: "https://kanban-fire-be71d-default-rtdb.firebaseio.com",
+    projectId: "kanban-fire-be71d",
+    storageBucket: "kanban-fire-be71d.appspot.com",
+    messagingSenderId: "817005516652",
+    appId: "1:817005516652:web:91bbbb904d017508b78549",
+    measurementId: "G-74MNGGSHGC"
+  },
 };
 
 /*
